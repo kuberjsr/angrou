4b8ee8ff6d9fc5cd562c9ef21d4c2499645279be
ramanvirtual <ramanaggarwal@virtualemployee.com>
2017-08-30 18:21:02 +0530

    route file update'

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -1,12 +1,12 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
+//import { AppComponent } from './app.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 
 export const router : Routes =[
-    {path : '', component : AppComponent},
-    {path : 'home', component : AppComponent},
+   // {path : '', component : AppComponent},
+    //{path : 'home', component : AppComponent},
     {path : 'about-us', component : AboutUsComponent},
     {path : 'contact-us', component : ContactUsComponent},
 ];
